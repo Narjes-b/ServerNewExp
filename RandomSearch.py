d4da8e97f6675a333d607f242d8a4329eff91914
narjes <narjes@gmail.com>
2024-02-21 20:35:20 -0500

    update

--- RandomSearch.py
+++ RandomSearch.py
@@ -273,7 +273,7 @@ def main(iterations, population_size,f):
 
 
 if __name__ == "__main__":
-    projects = ['eyp-systemd']
+    projects = ['secure_windows']
     Algo = 'RS_FINAL'
     for project in projects:
 
