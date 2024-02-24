4ab8c6b3492ba29921f5d13038219a6d87a62f7f
narjes <narjes@gmail.com>
2024-02-24 11:10:24 -0500

    update

--- PSO.py
+++ PSO.py
@@ -300,7 +300,7 @@ def main(num_particles, num_iterations,f):
 
 
 if __name__ == "__main__":
-    projects = ['infra']
+    projects = ['control-repo']
     Algo = 'PSO_FINAL'
     for project in projects:
 
