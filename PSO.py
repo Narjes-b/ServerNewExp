6dbb31d07bdc80425ff401cf2630e33dbc57d81b
narjes <narjes@gmail.com>
2024-02-26 13:32:52 -0500

    update

--- PSO.py
+++ PSO.py
@@ -300,7 +300,7 @@ def main(num_particles, num_iterations,f):
 
 
 if __name__ == "__main__":
-    projects = ['puppet-prometheus']
+    projects = ['iiab']
     Algo = 'PSO_FINAL'
     for project in projects:
 
