5470304421fb0c282efbe421fb4898011dedde4d
narjes <narjes@gmail.com>
2024-02-26 09:35:51 -0500

    update

--- RandomSearch.py
+++ RandomSearch.py
@@ -273,7 +273,7 @@ def main(iterations, population_size,f):
 
 
 if __name__ == "__main__":
-    projects = ['ansible-hardening']
+    projects = ['puppet-prometheus']
     Algo = 'RS_FINAL'
     for project in projects:
 
