0a4e0f26a8655089b5bafa0c79a53d555cb54d4a
narjes <narjes@gmail.com>
2024-03-06 23:06:04 -0500

    up

--- RandomSearch.py
+++ RandomSearch.py
@@ -273,7 +273,7 @@ def main(iterations, population_size,f):
 
 
 if __name__ == "__main__":
-    projects = ['puppet-tp']
+    projects = ['postgresql_cluster']
     Algo = 'RS_FINAL'
     for project in projects:
 
