18a15c3963f658919af481f3f150b479bd664078
narjes <narjes@gmail.com>
2024-03-24 15:23:27 -0400

    up

--- PSO.py
+++ PSO.py
@@ -300,7 +300,7 @@ def main(num_particles, num_iterations,f):
 
 
 if __name__ == "__main__":
-    projects = ['postgresql_cluster']
+    projects = ['eyp-systemd']
     Algo = 'PSO_FINAL'
     for project in projects:
 
